5deb173f40d89099c887d96c391a8ded3c059f71
attila00198 <attila00198@gmail.com>
2026-06-21 04:32:57 +0200

    feat: helper for json_encod()

--- src/router.php
+++ src/router.php
@@ -16,26 +16,26 @@ class Router
                     $data = $pc->get_all_persone();
 
                     header("Content-Type: application/json");
-                    echo json_encode(array("ok" => true, "data" => $data));
+                    echo $this->json(["ok" => true, "data" => $data]);
                     exit(0);
                 }
                 $pc = new PersoneController($this->db);
                 $data = $pc->get_persone_by_id($_GET["id"]);
 
                 header("Content-Type: application/json");
-                echo json_encode(array("ok" => true, "data" => $data));
+                echo $this->json(["ok" => true, "data" => $data]);
                 exit(0);
             } elseif ($method === "PUT") {
                 if (!isset($_GET["id"])) {
                     header("Content-Type: application/json");
-                    echo json_encode(["ok" => false, "message" => "Missing ID"]);
+                    echo $this->json(["ok" => false, "message" => "Missing ID"]);
                     exit(1);
                 }
 
                 $body = json_decode(file_get_contents("php://input"), true);
                 if (!$body) {
                     http_response_code(400);
-                    echo json_encode(["ok" => false, "error" => "Invalid body"]);
+                    echo $this->json(["ok" => false, "error" => "Invalid body"]);
                     exit(1);
                 }
 
@@ -43,16 +43,16 @@ class Router
                 $pc = new PersoneController($this->db);
                 if (!$pc->update_person($_GET["id"], $body)) {
                     http_response_code(400);
-                    echo json_encode(["ok" => false, "message" => "Something went wrong."]);
+                    echo $this->json(["ok" => false, "message" => "Something went wrong."]);
                     exit(1);
                 }
-                echo json_encode(["ok" => true, "message" => "success"]);
+                echo $this->json(["ok" => true, "message" => "success"]);
                 exit(0);
             } elseif ($method === "POST") {
                 $body = json_decode(file_get_contents("php://input"), true);
                 if (!$body) {
                     http_response_code(400);
-                    echo json_encode(["ok" => false, "error" => "Invalid body"]);
+                    echo $this->json(["ok" => false, "error" => "Invalid body"]);
                     exit(1);
                 }
 
@@ -60,9 +60,13 @@ class Router
                 $pc = new PersoneController($this->db);
                 $id = $pc->add_person($body);
 
-                echo json_encode(["ok" => true, "id" => $id]);
+                echo $this->json(["ok" => true, "id" => $id]);
                 exit(0);
             }
         }
+    }
+    private function json(array $data)
+    {
+        return json_encode($data);
     }
 }
